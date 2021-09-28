f327a2aea29a554b7166cd9263e9dc731687a091
Ke Shao <kshao1@emich.edu>
2021-09-28 13:55:22 +0000

    The first version of my website.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import Education from './components/education';
+import Contact from './components/contact';
+import Interests from './components/interests';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Education />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <React.StrictMode>
+    <Interests />
+  </React.StrictMode>,
+  document.getElementById('interest')
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Contact />
+  </React.StrictMode>,
+  document.getElementById('contact')
+);
